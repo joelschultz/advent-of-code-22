8498305a94050d485130f7c88af253c9b7fa767b
Daniel Månsson <daniel.mansson@resolutiongames.com>
2022-12-01 08:58:00 +0100

    Day 1

--- Days/Day1.cs
+++ Days/Day1.cs
@@ -1,22 +1,46 @@
 ﻿namespace Days {
     public class Day1 : DaySolverBase {
         public override string Example1 =>
-            @"";
+            @"1000
+2000
+3000
 
-        string Transform(string raw) {
-            return raw;
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+        List<List<int>> Transform(string raw) {
+            return raw
+                .Split("\n\n")
+                .Select(s => s.Split("\n")
+                    .Select(int.Parse)
+                    .ToList())
+                .ToList();
         }
 
         public override object Solve1(string raw) {
             var input = Transform(raw);
 
-            return -1;
+            return input
+                .Select(i => i.Sum())
+                .Max();
         }
 
         public override object Solve2(string raw) {
             var input = Transform(raw);
 
-            return -1;
+            return input
+                .Select(i => i.Sum())
+                .OrderByDescending(i => i)
+                .Take(3)
+                .Sum();;
         }
     }
 }
---
+++ Days/Day2.cs
@@ -0,0 +1,22 @@
+﻿namespace Days {
+    public class Day2 : DaySolverBase {
+        public override string Example1 =>
+            @"";
+
+        string Transform(string raw) {
+            return raw;
+        }
+
+        public override object Solve1(string raw) {
+            var input = Transform(raw);
+
+            return -1;
+        }
+
+        public override object Solve2(string raw) {
+            var input = Transform(raw);
+
+            return -1;
+        }
+    }
+}
